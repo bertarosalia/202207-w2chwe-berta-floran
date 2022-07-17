dcb053bd90e31d542a3b3ee089939cc51f2fe6a5
Berta Floran <bertafloran@hotmail.com>
2022-07-17 13:26:50 +0200

    create and test covidStatus function

---
+++ public/__test__/covidStatus.test.js
@@ -0,0 +1,22 @@
+describe("Given a function covidStatus that receives parameters", () => {
+  describe("When it´s called and receives number for counter and boolean status", () => {
+    test("Then it should return a boolean", () => {
+      let isAliveCovid = true;
+      const counter = 4;
+      const expectedResult = false;
+
+      const covidStatus = () => {
+        if ((isAliveCovid === true && counter < 2) || counter > 3) {
+          isAliveCovid = false;
+        } else if ((isAliveCovid === false && counter === 2) || counter === 3) {
+          isAliveCovid = true;
+        }
+        return isAliveCovid;
+      };
+
+      const result = covidStatus();
+
+      expect(result).toStrictEqual(expectedResult);
+    });
+  });
+});
---
+++ public/js/covidStatus.js
@@ -0,0 +1,13 @@
+let isAliveCovid = true;
+const counter = 2;
+
+const covidStatus = () => {
+  if ((isAliveCovid === true && counter < 2) || counter > 3) {
+    isAliveCovid = false;
+  } else if ((isAliveCovid === false && counter === 2) || counter === 3) {
+    isAliveCovid = true;
+  }
+  return isAliveCovid;
+};
+
+module.exports = covidStatus();
